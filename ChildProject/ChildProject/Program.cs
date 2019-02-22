de14346d67c45cbc17bf5bceff691fe2250ef403
nikolay.galushka <nikolay.galushka@betlab.com>
2019-02-22 16:57:38 +0200

    7

--- ChildProject/ChildProject/Program.cs
+++ ChildProject/ChildProject/Program.cs
@@ -7,7 +7,7 @@ namespace ChildProject
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello Child 6");
+            Console.WriteLine("Hello Child 7");
         }
     }
 }
