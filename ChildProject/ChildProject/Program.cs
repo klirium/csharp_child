d0eb2b4b0a706508d1b115c2008ecbe26eaafec9
nikolay.galushka <nikolay.galushka@betlab.com>
2019-02-22 11:35:13 +0200

    init

---
+++ ChildProject/ChildProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChildProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Child!");
+        }
+    }
+}
